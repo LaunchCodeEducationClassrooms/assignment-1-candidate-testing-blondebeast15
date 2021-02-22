4ef975b33dacdd656e998243c6f3f38d1f73d25f
blondebeast15 <mgreen5@live.maryville.edu>
2021-02-22 03:28:10 +0000

    reformatted output

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,9 @@ let candidateAnswer = "";
 let correct = 0; 
 let questions = [
   "Who was the first American woman in space? ",
-  "True or false: 5000 meters = 5 kilometers? ",
-  "(5 + 3) / 2 * 10 = ? ",
-  "Given the array [8, 'Orbit', 'Trajectory', '45'], what entry is at index 2? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   "What is the minimum crew size for the ISS? "
 ];
 let correctAnswers = [
